aa1be3b0ec44a577b02222e1dd09d00419728179
Kenneth Ekandem <kennyekandem@gmail.com>
2021-01-11 17:05:43 +0100

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,65 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    public function login(Request $request) {
 
-    public function login(Request $request)
-    {
-        try {
-            $request->validate([
-                'email' => 'email|required',
-                'password' => 'required'
-            ]);
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
 
-            $credentials = request(['email', 'password']);
+        $credentials = $request->only('email', 'password');
 
-            if (!Auth::attempt($credentials)) {
-                return response()->json([
-                    'status_code' => 401,
-                    'message' => 'Invalid login details'
-                ]);
-            }
+        $login = Auth::attempt($credentials);
 
-            $user = User::where('email', $request['email'])->first();
+        if(!$login) {
 
-            if ( ! Hash::check($request['password'], $user['password'], [])) {
-                return response()->json([
-                    'status_code' => 401,
-                    'message' => 'Password is incorrect'
-                ]);
-            }
-
-            $tokenResult = $user->createToken('authToken')->plainTextToken;
-
-            return response()->json([
-                'status_code' => 200,
-                'access_token' => $tokenResult,
-                'user' => $user,
-                'token_type' => 'Bearer',
-            ]);
-        } catch (ModelNotFoundException $error) {
             return response()->json([
                 'status_code' => 401,
-                'message' => 'User does not exist with this details',
-                'error' => $error,
+                'message' => 'Invalid login details'
             ]);
         }
-    }
-
-    public function index() {
-
-        $users = User::all();
 
         return response()->json([
-            'status_code' => 200,
-            'data' => $users,
+            'status_code' => 201,
+            'message' => 'User logged in'
         ]);
+
     }
 }
